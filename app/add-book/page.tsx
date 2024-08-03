d0e26f5103385a997f7d3a685ffbf19ca4622b71
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-08-03 07:03:23 +0530

    fix: minor bug fix

--- app/add-book/page.tsx
+++ app/add-book/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 import React from 'react';
 import BookForm from "@/components/BookForm";
+import {useSelector} from "react-redux";
+import {RootState} from "@/store/store";
+import {useRouter} from "next/navigation";
 
 const AddBook = () => {
+    const auth = useSelector((state: RootState) => state.auth.authState);
+    const router = useRouter()
 
+    if(!auth) router.push('/login')
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-3xl font-bold mb-4">Add a New Book</h1>
